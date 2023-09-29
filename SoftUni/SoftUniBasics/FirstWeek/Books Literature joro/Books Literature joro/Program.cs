ecf92d6a953a7104afbec9aa3300f5aa8cdbf134
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 11:54:34 +0300

    Add files via upload

---
+++ SoftUni/SoftUniBasics/FirstWeek/Books Literature joro/Books Literature joro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Books_Literature_joro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPagesOfTheCurrentBook = int.Parse (Console.ReadLine());
+            int numberHeReadsPerHour = int.Parse (Console.ReadLine());
+            int numberOfDaysInWhichHeNeedsToReadTheBook = int.Parse (Console.ReadLine());
+
+            int hours = numberOfPagesOfTheCurrentBook / numberHeReadsPerHour / numberOfDaysInWhichHeNeedsToReadTheBook;
+            Console.WriteLine (hours);
+        }
+    }
+}
