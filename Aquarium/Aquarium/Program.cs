31d0ab1d95d48ccc0a04b1b73bff3355cd973276
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 11:28:08 +0300

    Add files via upload

---
+++ Aquarium/Aquarium/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input cm make it in dm by deviding by 10
+            double lenght = double.Parse (Console.ReadLine())/10;
+            double width = double.Parse (Console.ReadLine())/10;
+            double height = double.Parse (Console.ReadLine())/10;
+            //filled means all things tht fill the aquarium additionally such as pump
+            double percentageoffilled = double.Parse (Console.ReadLine())/100.0;
+            //calculate liters
+            double space =(lenght * width * height) * (1 - percentageoffilled);
+            Console.WriteLine(space);
+        }
+    }
+}
