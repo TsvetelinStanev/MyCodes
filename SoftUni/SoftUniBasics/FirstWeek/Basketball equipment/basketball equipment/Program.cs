b2c24e67cca4586b0da32ee6a5ac40c2c53fa8b2
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 11:53:38 +0300

    Add files via upload

---
+++ SoftUni/SoftUniBasics/FirstWeek/Basketball equipment/basketball equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace basketball_equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearlyTax = int.Parse (Console.ReadLine());
+            //Price of equipment
+            double priceforshoees = yearlyTax * 0.60;
+            double outfitekip = priceforshoees * 0.80;
+            double basketball = outfitekip * 0.25;
+            double accessories = basketball * 0.20;
+            //Summing everything up
+            double everything = yearlyTax + priceforshoees + outfitekip + accessories + basketball;
+            Console.WriteLine(everything);
+        }
+    }
+}
