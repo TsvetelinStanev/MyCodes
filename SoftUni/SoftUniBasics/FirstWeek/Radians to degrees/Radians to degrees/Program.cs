da2b389a6ab8c9928f46b3f918b6adc4f7b26496
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 11:59:55 +0300

    Add files via upload

---
+++ SoftUni/SoftUniBasics/FirstWeek/Radians to degrees/Radians to degrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radians_to_degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angleInRadians = double.Parse (Console.ReadLine());
+            double degrees = angleInRadians * 180 / Math.PI;
+            Console.WriteLine(degrees);
+        }
+    }
+}
---
+++ SoftUni/SoftUniBasics/FirstWeek/Repainting/Repainting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double safetyNailonPerSquareMeeter = 1.50;
+            double paintPerLiter = 14.50;
+            int razreditelThinnerInEnglish = 5;
+            //Input
+            int nailon = int.Parse (Console.ReadLine())+2;
+            int paint = int.Parse (Console.ReadLine());
+            double realPaint = paint * (110.0 / 100.0);
+            int Thinner = int.Parse(Console.ReadLine());
+            int hoursTodoTheWork = int.Parse (Console.ReadLine());
+            //money to be spend
+            double torbichkisuma = 0.40;
+            double sumforPaint = paintPerLiter * realPaint;
+            double sumforThinner = razreditelThinnerInEnglish * Thinner;
+            double sumforNailon = nailon * safetyNailonPerSquareMeeter;
+            //sum for every material and than workforce calculated 
+            double sumofeverymaterial = sumforNailon + sumforPaint + sumforThinner + torbichkisuma;
+            double payforworkforce = sumofeverymaterial * 0.30 * hoursTodoTheWork;
+            double everything = sumofeverymaterial + payforworkforce;
+            Console.WriteLine(everything);
+        }
+    }
+}
