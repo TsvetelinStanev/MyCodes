1e4ea5c86cfbc7bedc1e0eacee57360dd562a565
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 17:18:12 +0300

    Add files via upload

---
+++ SoftUni/SoftUniBasics/Conditional Statements - Lab/Number 100-200/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Number_100_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //write a programm which checks if a number is under 100, 100-200 or over 200
+            int number = int.Parse (Console.ReadLine());
+
+            if (number < 100) 
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (number <= 200)
+            {
+                Console.WriteLine("Between 100 and 200"); 
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
---
+++ SoftUni/SoftUniBasics/Conditional Statements - Lab/Speed Info/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //write a programm about speed limit (not a whole number) inserted from the client
+            double speed = double.Parse(Console.ReadLine());
+
+            //if it is between 0 and 10 write "slow", 50 till 150 fast, 150 till 1000 ulta fast , above 1000 exttremely fast
+            if (speed <= 10) 
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed <= 50) 
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast"); 
+            }
+        }
+    }
+}
