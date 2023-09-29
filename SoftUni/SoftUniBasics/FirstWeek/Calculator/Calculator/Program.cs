ac26e7d8d9756a16559d69ec2ecf89a6049ab5c6
TsvetelinStanev <145497982+TsvetelinStanev@users.noreply.github.com>
2023-09-29 11:55:39 +0300

    Add files via upload

---
+++ SoftUni/SoftUniBasics/FirstWeek/Calculator/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse (Console.ReadLine());
+            int timeOfdepositInMonths = int.Parse (Console.ReadLine ());
+            double yearlyInterestRate = double.Parse (Console.ReadLine ());
+            double yearlyInterestRateInPercentage = yearlyInterestRate / 100;
+            double sum = deposit + timeOfdepositInMonths * ((deposit * yearlyInterestRateInPercentage)/12);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ SoftUni/SoftUniBasics/FirstWeek/Concaneted data/Concaneted data/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Concaneted_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = int.Parse (Console.ReadLine());
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {city}.");
+
+        }
+    }
+}
